4a6854ab874232450df8a933d341b9ddbd27e2b2
MarvinKweyu <mkweyu1@gmail.com>
2024-09-06 18:35:12 +0300

    style: navigation

--- src/app/loading.js
+++ src/app/loading.js
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 function Loading() {
   return (
-      <div className='w-full bg-black min-h-screen'>
+      <div className='w-full min-h-screen'>
       
       <Skeleton />
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,11 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <div>
-        
         <p className="text-4xl">Welcome to Chef&apos;s Kitchen</p>
-        <Link href={"/recipe"} className="text-center">Explore Recipes</Link>
-        
+        <p className="text-center underline mt-2">
+
+        <Link href={"/recipe"}>Explore Recipes</Link> 
+        </p>
 </div>
     </div>
   );
